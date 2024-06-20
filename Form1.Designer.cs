0687b632fd2f1b6fda036f9252d8736fb9761c3b
DirectMJ <138039347+DirectMJ@users.noreply.github.com>
2024-06-21 06:07:42 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace kape
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cb1 = new CheckBox();
+            cb2 = new CheckBox();
+            cb3 = new CheckBox();
+            cb4 = new CheckBox();
+            cb5 = new CheckBox();
+            cb6 = new CheckBox();
+            cb7 = new CheckBox();
+            cb8 = new CheckBox();
+            numUd = new NumericUpDown();
+            numUd8 = new NumericUpDown();
+            numUd5 = new NumericUpDown();
+            numUd7 = new NumericUpDown();
+            numUd6 = new NumericUpDown();
+            numUd4 = new NumericUpDown();
+            numUd3 = new NumericUpDown();
+            numUd2 = new NumericUpDown();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            cb9 = new CheckBox();
+            btnCo = new Button();
+            rb1 = new RadioButton();
+            rb2 = new RadioButton();
+            lblttl = new Label();
+            lblptns = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numUd).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numUd8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numUd5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numUd7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numUd6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numUd4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numUd3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numUd2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // cb1
+            // 
+            cb1.AutoSize = true;
+            cb1.Location = new Point(221, 225);
+            cb1.Name = "cb1";
+            cb1.Size = new Size(210, 29);
+            cb1.TabIndex = 0;
+            cb1.Text = "Creamy Caramel Latte";
+            cb1.UseVisualStyleBackColor = true;
+            cb1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // cb2
+            // 
+            cb2.AutoSize = true;
+            cb2.Location = new Point(221, 280);
+            cb2.Name = "cb2";
+            cb2.Size = new Size(241, 29);
+            cb2.TabIndex = 1;
+            cb2.Text = "Salted Caramel Cold Brew";
+            cb2.UseVisualStyleBackColor = true;
+            cb2.CheckedChanged += cb2_CheckedChanged;
+            // 
+            // cb3
+            // 
+            cb3.AutoSize = true;
+            cb3.Location = new Point(221, 333);
+            cb3.Name = "cb3";
+            cb3.Size = new Size(281, 29);
+            cb3.TabIndex = 2;
+            cb3.Text = "Vanilla Sweet Cream Cold Brew";
+            cb3.UseVisualStyleBackColor = true;
+            cb3.CheckedChanged += cb3_CheckedChanged;
+            // 
+            // cb4
+            // 
+            cb4.AutoSize = true;
+            cb4.Location = new Point(221, 389);
+            cb4.Name = "cb4";
+            cb4.Size = new Size(259, 29);
+            cb4.TabIndex = 3;
+            cb4.Text = "Burnt Caramel Oatmilk Latte\r\n";
+            cb4.UseVisualStyleBackColor = true;
+            cb4.CheckedChanged += cb4_CheckedChanged;
+            // 
+            // cb5
+            // 
+            cb5.AutoSize = true;
+            cb5.Location = new Point(748, 225);
+            cb5.Name = "cb5";
+            cb5.Size = new Size(188, 29);
+            cb5.TabIndex = 4;
+            cb5.Text = "Hot Brewed Coffee";
+            cb5.UseVisualStyleBackColor = true;
+            cb5.CheckedChanged += cb5_CheckedChanged;
+            // 
+            // cb6
+            // 
+            cb6.AutoSize = true;
+            cb6.Location = new Point(748, 278);
+            cb6.Name = "cb6";
+            cb6.Size = new Size(226, 29);
+            cb6.TabIndex = 5;
+            cb6.Text = "Iced Caramel Macchiato";
+            cb6.UseVisualStyleBackColor = true;
+            cb6.CheckedChanged += cb6_CheckedChanged;
+            // 
+            // cb7
+            // 
+            cb7.AutoSize = true;
+            cb7.Location = new Point(748, 331);
+            cb7.Name = "cb7";
+            cb7.Size = new Size(257, 29);
+            cb7.TabIndex = 6;
+            cb7.Text = "Green Tea Cream Capuccino";
+            cb7.UseVisualStyleBackColor = true;
+            cb7.CheckedChanged += cb7_CheckedChanged;
+            // 
+            // cb8
+            // 
+            cb8.AutoSize = true;
+            cb8.Location = new Point(748, 387);
+            cb8.Name = "cb8";
+            cb8.Size = new Size(214, 29);
+            cb8.TabIndex = 7;
+            cb8.Text = "Java Chip Frappuccino";
+            cb8.UseVisualStyleBackColor = true;
+            cb8.CheckedChanged += cb8_CheckedChanged;
+            // 
+            // numUd
+            // 
+            numUd.Enabled = false;
+            numUd.Location = new Point(149, 224);
+            numUd.Name = "numUd";
+            numUd.Size = new Size(57, 31);
+            numUd.TabIndex = 8;
+            numUd.ValueChanged += numUd_ValueChanged;
+            // 
+            // numUd8
+            // 
+            numUd8.Enabled = false;
+            numUd8.Location = new Point(676, 385);
+            numUd8.Name = "numUd8";
+            numUd8.Size = new Size(57, 31);
+            numUd8.TabIndex = 9;
+            // 
+            // numUd5
+            // 
+            numUd5.Enabled = false;
+            numUd5.Location = new Point(676, 223);
+            numUd5.Name = "numUd5";
+            numUd5.Size = new Size(57, 31);
+            numUd5.TabIndex = 10;
+            // 
+            // numUd7
+            // 
+            numUd7.Enabled = false;
+            numUd7.Location = new Point(676, 329);
+            numUd7.Name = "numUd7";
+            numUd7.Size = new Size(57, 31);
+            numUd7.TabIndex = 11;
+            // 
+            // numUd6
+            // 
+            numUd6.Enabled = false;
+            numUd6.Location = new Point(676, 276);
+            numUd6.Name = "numUd6";
+            numUd6.Size = new Size(57, 31);
+            numUd6.TabIndex = 12;
+            // 
+            // numUd4
+            // 
+            numUd4.Enabled = false;
+            numUd4.Location = new Point(149, 387);
+            numUd4.Name = "numUd4";
+            numUd4.Size = new Size(57, 31);
+            numUd4.TabIndex = 13;
+            // 
+            // numUd3
+            // 
+            numUd3.Enabled = false;
+            numUd3.Location = new Point(149, 331);
+            numUd3.Name = "numUd3";
+            numUd3.Size = new Size(57, 31);
+            numUd3.TabIndex = 14;
+            // 
+            // numUd2
+            // 
+            numUd2.Enabled = false;
+            numUd2.Location = new Point(149, 276);
+            numUd2.Name = "numUd2";
+            numUd2.Size = new Size(57, 31);
+            numUd2.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(97, 607);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(305, 255);
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(165, 621);
+            label1.Name = "label1";
+            label1.Size = new Size(169, 25);
+            label1.TabIndex = 17;
+            label1.Text = "Membership Dining";
+            // 
+            // cb9
+            // 
+            cb9.AutoSize = true;
+            cb9.Location = new Point(113, 667);
+            cb9.Name = "cb9";
+            cb9.Size = new Size(105, 29);
+            cb9.TabIndex = 18;
+            cb9.Text = "Member";
+            cb9.UseVisualStyleBackColor = true;
+            // 
+            // btnCo
+            // 
+            btnCo.Location = new Point(676, 607);
+            btnCo.Name = "btnCo";
+            btnCo.Size = new Size(329, 71);
+            btnCo.TabIndex = 19;
+            btnCo.Text = "Complete Order";
+            btnCo.UseVisualStyleBackColor = true;
+            btnCo.Click += btnCo_Click;
+            // 
+            // rb1
+            // 
+            rb1.AutoSize = true;
+            rb1.Location = new Point(113, 736);
+            rb1.Name = "rb1";
+            rb1.Size = new Size(102, 29);
+            rb1.TabIndex = 20;
+            rb1.TabStop = true;
+            rb1.Text = "For here";
+            rb1.UseVisualStyleBackColor = true;
+            rb1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // rb2
+            // 
+            rb2.AutoSize = true;
+            rb2.Location = new Point(113, 786);
+            rb2.Name = "rb2";
+            rb2.Size = new Size(82, 29);
+            rb2.TabIndex = 21;
+            rb2.TabStop = true;
+            rb2.Text = "To go";
+            rb2.UseVisualStyleBackColor = true;
+            // 
+            // lblttl
+            // 
+            lblttl.AutoSize = true;
+            lblttl.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblttl.Location = new Point(676, 713);
+            lblttl.Name = "lblttl";
+            lblttl.Size = new Size(103, 45);
+            lblttl.TabIndex = 22;
+            lblttl.Text = "Total:";
+            // 
+            // lblptns
+            // 
+            lblptns.AutoSize = true;
+            lblptns.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblptns.Location = new Point(676, 790);
+            lblptns.Name = "lblptns";
+            lblptns.Size = new Size(122, 45);
+            lblptns.TabIndex = 23;
+            lblptns.Text = "Points:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("MS PGothic", 28F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(97, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(575, 56);
+            label2.TabIndex = 24;
+            label2.Text = "BucksStar Coffee Shop\r\n";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1408, 920);
+            Controls.Add(label2);
+            Controls.Add(lblptns);
+            Controls.Add(lblttl);
+            Controls.Add(rb2);
+            Controls.Add(rb1);
+            Controls.Add(btnCo);
+            Controls.Add(cb9);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(numUd2);
+            Controls.Add(numUd3);
+            Controls.Add(numUd4);
+            Controls.Add(numUd6);
+            Controls.Add(numUd7);
+            Controls.Add(numUd5);
+            Controls.Add(numUd8);
+            Controls.Add(numUd);
+            Controls.Add(cb8);
+            Controls.Add(cb7);
+            Controls.Add(cb6);
+            Controls.Add(cb5);
+            Controls.Add(cb4);
+            Controls.Add(cb3);
+            Controls.Add(cb2);
+            Controls.Add(cb1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numUd).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numUd8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numUd5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numUd7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numUd6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numUd4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numUd3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numUd2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox cb1;
+        private CheckBox cb2;
+        private CheckBox cb3;
+        private CheckBox cb4;
+        private CheckBox cb5;
+        private CheckBox cb6;
+        private CheckBox cb7;
+        private CheckBox cb8;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown5;
+        private NumericUpDown numericUpDown6;
+        private NumericUpDown numericUpDown7;
+        private NumericUpDown numericUpDown8;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private CheckBox cb9;
+        private Button btnCo;
+        private RadioButton rb1;
+        private RadioButton rb2;
+        private Label lblttl;
+        private Label lblptns;
+        private NumericUpDown numUd;
+        private NumericUpDown numUd8;
+        private NumericUpDown numUd5;
+        private NumericUpDown numUd7;
+        private NumericUpDown numUd6;
+        private NumericUpDown numUd4;
+        private NumericUpDown numUd3;
+        private NumericUpDown numUd2;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+namespace kape
+{
+    public partial class Form1 : Form
+    {
+        double Item1 = 120.00;
+        double Item2 = 110.00;
+        double Item3 = 125.00;
+        double Item4 = 150.00;
+        double Item5 = 90.00;
+        double Item6 = 135.00;
+        double Item7 = 155.00;
+        double Item8 = 162.00;
+        double pointsEarned;
+        double Total = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb1.Checked)
+            {
+                numUd.Enabled = true;
+            }
+            else
+            {
+                numUd.Enabled = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cb2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb2.Checked)
+            {
+                numUd2.Enabled = true;
+            }
+            else
+            {
+                numUd2.Enabled = false;
+            }
+        }
+
+        private void cb3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb3.Checked)
+            {
+                numUd3.Enabled = true;
+            }
+            else
+            {
+                numUd3.Enabled = false;
+            }
+        }
+
+        private void cb4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb4.Checked)
+            {
+                numUd4.Enabled = true;
+            }
+            else
+            {
+                numUd4.Enabled = false;
+            }
+        }
+
+        private void cb5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb5.Checked)
+            {
+                numUd5.Enabled = true;
+            }
+            else
+            {
+                numUd5.Enabled = false;
+            }
+        }
+
+        private void cb6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb6.Checked)
+            {
+                numUd6.Enabled = true;
+            }
+            else
+            {
+                numUd6.Enabled = false;
+            }
+        }
+
+        private void cb7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb7.Checked)
+            {
+                numUd7.Enabled = true;
+            }
+            else
+            {
+                numUd7.Enabled = false;
+            }
+        }
+
+        private void cb8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb8.Checked)
+            {
+                numUd8.Enabled = true;
+            }
+            else
+            {
+                numUd8.Enabled = false;
+            }
+        }
+
+        private void btnCo_Click(object sender, EventArgs e)
+        {
+            double orderTotal = 0;
+
+            if (!cb1.Checked && !cb2.Checked && !cb3.Checked && !cb4.Checked && !cb5.Checked && !cb6.Checked && !cb7.Checked && !cb8.Checked)
+            {
+                MessageBox.Show("Select an Item");
+            }
+            else if (!rb1.Checked && !rb2.Checked)
+            {
+                MessageBox.Show("Select Dining Method");
+            }
+            else
+            {
+                orderTotal = computeOrders(orderTotal);
+
+                if (rb2.Checked)
+                {
+                    orderTotal += 10;
+                }
+
+                if (cb9.Checked)
+                {
+                    orderTotal -= 50;
+                    pointsEarned = orderTotal * 0.03;
+                    lblptns.Text = "Points: " + pointsEarned.ToString();
+                }
+                else if (!cb9.Checked)
+                {
+                    lblptns.Text = "Points: ";
+                }
+                if (orderTotal != Total)
+                {
+                    Total = orderTotal;
+                    lblttl.Text = "Total: " + Total.ToString();
+                }
+
+            }
+        }
+        public double computeOrders(double orderTotal)
+        {
+            if (cb1.Checked)
+            {
+                orderTotal += Item1 * double.Parse(numUd.Value.ToString());
+            }
+            if (cb2.Checked)
+            {
+                orderTotal += Item2 * double.Parse(numUd2.Value.ToString());
+            }
+            if (cb3.Checked)
+            {
+                orderTotal += Item3 * double.Parse(numUd3.Value.ToString());
+            }
+            if (cb4.Checked)
+            {
+                orderTotal += Item4 * double.Parse(numUd4.Value.ToString());
+            }
+            if (cb5.Checked)
+            {
+                orderTotal += Item5 * double.Parse(numUd5.Value.ToString());
+            }
+            if (cb6.Checked)
+            {
+                orderTotal += Item6 * double.Parse(numUd6.Value.ToString());
+            }
+            if (cb7.Checked)
+            {
+                orderTotal += Item7 * double.Parse(numUd7.Value.ToString());
+            }
+            if (cb8.Checked)
+            {
+                orderTotal += Item8 * double.Parse(numUd8.Value.ToString());
+            }
+            return orderTotal;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numUd_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+
+}
+
